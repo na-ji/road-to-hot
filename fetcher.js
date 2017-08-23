c17f932b9265b3041738ec40b774976b84eece38
Naji Astier <naji.demolitionman@gmail.com>
2017-08-23 13:03:49 +0200

    Update dependencies

--- fetcher.js
+++ fetcher.js
@@ -1,35 +1,39 @@
-var request = require('request');
-var cheerio = require('cheerio');
-var _       = require('lodash');
+const request = require('request');
+const cheerio = require('cheerio');
+const _       = require('lodash');
 
-var headers = {
+const headers = {
 	'Accept'           : 'application/json, text/javascript, */*; q=0.01',
 	'Referer'          : 'http://9gag.com/fresh',
-	'User-Agent'       : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/53.0.2785.143 Chrome/53.0.2785.143 Safari/537.36',
+	'User-Agent'       : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/60.0.3112.78 Chrome/60.0.3112.78 Safari/537.36',
 	'X-Requested-With' : 'XMLHttpRequest'
 };
 
-var parseArticles = function(rawHTML){
+const parseArticles = function(rawHTML){
 	let $ = cheerio.load(rawHTML);
-	$('article.badge-entry-container').each(function(i, article) {
-		// console.log($(this).attr('data-entry-id'));
-		// console.log($(this).attr('data-entry-url'));
-		// console.log($(this).attr('data-entry-votes'));
-		// console.log($(this).attr('data-entry-comments'));
-		console.log($(this).children('header').text().trim());
+
+	$('article.badge-entry-container').each(function() {
+		let post = {
+			id: $(this).attr('data-entry-id'),
+			url: $(this).attr('data-entry-url'),
+			votes: $(this).attr('data-entry-votes'),
+			comments: $(this).attr('data-entry-comments'),
+			title: $(this).children('header').text().trim()
+		};
+
+		console.log(post);
 	});
 };
 
-var syncFetch = function(url, times, max) {
+const syncFetch = function(url, times, max) {
 	request.get({
 		url     : url,
 		headers : headers
 	}, function(err, httpResponse, body) {
 		let response = JSON.parse(body);
 
-		_.forEach(response.items, function(item, id) {
-			let $ = cheerio.load(item);
-			console.log($('article.badge-entry-container').children('header').text().trim());
+		_.forEach(response.items, function(item) {
+            parseArticles(item);
 		});
 
 		console.log(response.loadMoreUrl);
@@ -45,15 +49,9 @@ var syncFetch = function(url, times, max) {
 request.get({
 	url: 'http://9gag.com/fresh'
 }, function(err, httpResponse, body) {
-	let $ = cheerio.load(body);
-	$('article.badge-entry-container').each(function(i, article) {
-		// console.log($(this).attr('data-entry-id'));
-		// console.log($(this).attr('data-entry-url'));
-		// console.log($(this).attr('data-entry-votes'));
-		// console.log($(this).attr('data-entry-comments'));
-		console.log($(this).children('header').text().trim());
-	});
-	console.log($('.badge-load-more-post').attr('href'));
+    let $ = cheerio.load(body);
+    parseArticles(body);
+	console.log($('.badge-load-smore-post').attr('href'));
 
 	syncFetch('http://9gag.com' + $('.badge-load-more-post').attr('href'), 0, 3);
 });
